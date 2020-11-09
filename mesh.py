1ff31f7636e4cb5d0d4faa83ef1a5c8f7903aba1
r_vizzz <>
2020-11-09 10:52:34 -0500

    Initial commit

---
+++ mesh.py
@@ -0,0 +1,470 @@
+import numpy as np
+
+class Mesh:
+
+    def __init__(self):
+        self.vertices = []
+        self.edges = []
+        self.faces = []
+        
+        self.vertex_dict = {}
+        self.edge_dict = {}
+    
+    def num_vertices(self):
+        return len(self.vertices)
+        
+    def num_edges(self):
+        return len(self.edges)
+        
+    def num_faces(self):
+        return len(self.faces)
+    
+    def print_edges(self):
+        
+        for e in self.edges:
+            print(str(e.adj_vertices[0].coor) + " " + str(e.adj_vertices[1].coor))
+    
+    def add_face_by_points(self, points):
+        
+        face_vertices = []
+        
+        for p in points:
+            face_vertices.append(self.add_vertex(p))
+            
+        self.add_face(face_vertices)
+    
+    def add_face_by_indicies(self, indices):
+        
+        face_vertices = []
+        
+        for i in indices:
+            face_vertices.append(self.vertices[i])
+            
+        self.add_face(face_vertices)
+    
+    def add_face(self, face_vertices):
+    
+        #make face edges
+        face_edges = []
+    
+        for i in range(len(face_vertices)):
+            
+            v1 = face_vertices[i]
+            v2 = face_vertices[(i + 1) % len(face_vertices)]
+            
+            face_edges.append(self.add_edge(v1, v2))
+    
+        face = Face(face_vertices, face_edges)
+        self.faces.append(face)
+        
+        #add face to vertices
+        for v in face_vertices:
+            v.add_face(face)
+            
+        #add face to edges
+        for e in face_edges:
+            e.add_face(face)
+            
+        #ignore adjacent faces
+        
+            
+    def add_edge(self, v1, v2):
+                
+        v1t = tuple(v1.coor.tolist())
+        v2t = tuple(v2.coor.tolist())
+        
+        if (v1t, v2t) in self.edge_dict:
+            return self.edges[self.edge_dict[(v1t, v2t)]]
+        
+        new_edge = Edge(v1, v2)
+        self.edges.append(new_edge)
+        
+        id = len(self.edges) - 1
+        
+        self.edge_dict[(v1t, v2t)] = id
+        self.edge_dict[((v2t, v1t))] = id
+        
+        v1.add_edge(new_edge)
+        v2.add_edge(new_edge)
+        
+        v1.add_vertex(v2)
+        v2.add_vertex(v1)
+        
+        return new_edge
+            
+    def add_vertex(self, p):
+        
+        pt = tuple(p.tolist())
+        
+        if pt in self.vertex_dict:
+            return self.vertices[self.vertex_dict[pt]]
+        
+        new_vertex = Vertex(p)
+        
+        self.vertices.append(new_vertex)
+        id = len(self.vertices) - 1
+        
+        self.vertex_dict[pt] = id
+        
+        new_vertex.set_id(id)
+        
+        return new_vertex
+
+    def compute_catmull_clark_surface(self):
+        
+        cc_mesh = Mesh()
+        
+        cc_animate_mesh = MeshAnimate()
+        
+        for f in self.faces:
+            f.compute_catmull_clark_point()
+            
+        for e in self.edges:
+            e.compute_catmull_clark_point()
+            
+        for v in self.vertices:
+            v.compute_catmull_clark_point()
+            
+        for f in self.faces:
+        
+            polys = []
+            
+            for i in range(len(f.adj_edges)):
+                e1_point = f.adj_edges[(i - 1) % len(f.adj_edges)].cc_point
+                e2_point = f.adj_edges[i].cc_point
+                v_point = f.adj_vertices[i].cc_point
+                f_point = f.cc_point
+                
+                poly = [e1_point, v_point, e2_point, f_point]
+                polys.append(poly)
+                
+                cc_mesh.add_face_by_points(poly)
+                
+            face_point_pairs = []
+            
+            for i in range(len(f.adj_edges)):
+                pp = [f.adj_vertices[i].coor, f.cc_point]
+                face_point_pairs.append(pp)
+            
+            cc_animate_mesh.add_face_by_points(face_point_pairs)
+            
+            for i in range(len(f.adj_edges)):
+                
+                v1_point = f.adj_vertices[i].coor
+                v2_point = f.adj_vertices[(i + 1) % len(f.adj_vertices)].coor
+                
+                e_point = f.adj_edges[i].cc_point
+                
+                f_point = f.cc_point
+                
+                edge_point_pairs = [[v1_point, e_point], [v2_point, e_point], [v2_point, f_point], [v1_point, f_point]]
+                
+                cc_animate_mesh.add_face_by_points(edge_point_pairs)
+                
+            for i in range(len(f.adj_edges)):
+            
+                vertex_point_pairs = []
+                
+                for p in polys[i]:
+                    vertex_point_pairs.append([f.adj_vertices[i].coor, p])
+                    
+                cc_animate_mesh.add_face_by_points(vertex_point_pairs)
+                
+        
+        return (cc_mesh, cc_animate_mesh)
+        
+    def compute_doo_sabin_surface(self, clamp = False):
+        
+        ds_mesh = Mesh()
+        ds_animate_mesh = MeshAnimate()
+        
+        for f in self.faces:
+            f.compute_doo_sabin_face_point()
+            
+        for e in self.edges:
+            e.compute_doo_sabin_point()
+            
+        for f in self.faces:
+            f.compute_doo_sabin_points()
+            
+        #generate face-faces
+        for f in self.faces:
+            ds_mesh.add_face_by_points(f.ds_points)
+            
+        #generate edge-faces
+        for e in self.edges:
+            
+            if e.on_boundary():
+                continue
+            
+            f1 = e.adj_faces[0]
+            f2 = e.adj_faces[1]
+            
+            f1_index = f1.adj_edges.index(e)
+            f2_index = f2.adj_edges.index(e)
+            
+            poly = []
+            point_pairs = []
+            
+            poly.append(f1.ds_points[(f1_index + 1) % len(f1.ds_points)])
+            poly.append(f1.ds_points[f1_index])
+            poly.append(f2.ds_points[(f2_index + 1) % len(f2.ds_points)])
+            poly.append(f2.ds_points[f2_index])
+            
+            point_pairs.append([f1.adj_vertices[(f1_index + 1) % len(f1.adj_vertices)].coor, poly[0]])
+            point_pairs.append([f1.adj_vertices[f1_index].coor, poly[1]])
+            point_pairs.append([f2.adj_vertices[(f2_index + 1) % len(f2.adj_vertices)].coor, poly[2]])
+            point_pairs.append([f2.adj_vertices[f2_index].coor, poly[3]])
+            
+            ds_mesh.add_face_by_points(poly)
+            ds_animate_mesh.add_face_by_points(point_pairs)
+            
+        #generate vertex-faces
+        for v in self.vertices:
+            
+            if v.on_boundary():
+                continue
+                
+            start_f = v.adj_faces[0]
+            f = start_f
+            
+            poly = []
+            point_pairs = []
+            
+            while True:
+        
+                index = f.adj_vertices.index(v)
+                
+                poly.append(f.ds_points[index])
+                point_pairs.append([v.coor, f.ds_points[index]])
+                
+                e = f.adj_edges[(index - 1) % len(f.ds_points)]
+                
+                if e.adj_faces[0] == f:
+                    f = e.adj_faces[1]
+                else:
+                    f = e.adj_faces[0]
+            
+                if f == start_f:
+                    break
+            
+            ds_mesh.add_face_by_points(poly)
+            ds_animate_mesh.add_face_by_points(point_pairs)
+            
+        for f in self.faces:
+            
+            point_pairs = []
+            
+            for i in range(len(f.adj_vertices)):
+                pp = [f.adj_vertices[i].coor, f.ds_points[i]]
+                point_pairs.append(pp)
+                
+            ds_animate_mesh.add_face_by_points(point_pairs)
+        
+        
+        return (ds_mesh, ds_animate_mesh)
+
+class MeshAnimate(Mesh):
+    
+    def __init__(self):
+        super().__init__()
+        
+    def add_vertex(self, p, q):
+        
+        t = (tuple(p.tolist()), tuple(q.tolist()))
+        if t in self.vertex_dict:
+            return self.vertices[self.vertex_dict[t]]
+        
+        new_vertex = VertexAnimate(p, q)
+        self.vertices.append(new_vertex)
+        
+        id = len(self.vertices) - 1
+        
+        self.vertex_dict[t] = id
+        
+        new_vertex.set_id(id)
+        
+        return new_vertex
+        
+    def add_edge(self, v1, v2):
+                
+        v1t = (tuple(v1.coor_1.tolist()), tuple(v1.coor_2.tolist()))
+        v2t = (tuple(v2.coor_1.tolist()), tuple(v2.coor_2.tolist()))
+        
+        if (v1t, v2t) in self.edge_dict:
+            return self.edges[self.edge_dict[(v1t, v2t)]]
+        
+        new_edge = Edge(v1, v2)
+        self.edges.append(new_edge)
+        
+        id = len(self.edges) - 1
+        
+        self.edge_dict[(v1t, v2t)] = id
+        self.edge_dict[((v2t, v1t))] = id
+        
+        v1.add_edge(new_edge)
+        v2.add_edge(new_edge)
+        
+        v1.add_vertex(v2)
+        v2.add_vertex(v1)
+        
+        return new_edge
+        
+    def add_face_by_points(self, point_pairs):
+        
+        face_vertices = []
+        
+        for pp in point_pairs:
+            face_vertices.append(self.add_vertex(pp[0], pp[1]))
+            
+        self.add_face(face_vertices)
+
+class TopoElement:
+    
+    def __init__(self):
+        self.adj_vertices = []
+        self.adj_edges = []
+        self.adj_faces = []
+        self.clamp_to_boundary = False
+    
+    def add_vertex(self, v):
+        self.adj_vertices.append(v)
+    
+    def add_edge(self, e):
+        self.adj_edges.append(e)
+    
+    def add_face(self, f):
+        self.adj_faces.append(f)
+
+    def get_adj_vertices():
+        return self.adj_vertices
+    
+    def get_adj_edges():
+        return self.adj_edges
+    
+    def get_adj_faces():
+        return self.adj_faces
+
+
+class VertexAnimate(TopoElement):
+
+    def __init__(self, start, end):
+        super().__init__()
+        
+        self.coor_1 = start
+        self.coor_2 = end
+        
+    def set_id(self, i):
+        self.id = i
+        
+    def get_val(self, t = 0):
+        return (1 - t) * self.coor_1 + t * self.coor_2
+
+class Vertex(TopoElement):
+    
+    def __init__(self, p):
+        super().__init__()
+        
+        self.coor = p[:]
+        
+    def set_id(self, i):
+        self.id = i
+    
+    def compute_catmull_clark_point(self):
+        
+        if self.on_boundary():
+            self.cc_point = self.coor[:]
+        else:
+        
+            num_points = len(self.adj_vertices)
+        
+            f_point = np.sum([f.cc_point for f in self.adj_faces], axis = 0) / len(self.adj_faces)
+            e_point = (np.sum([v.coor for v in self.adj_vertices], axis = 0) + (self.coor * num_points))
+            e_point = e_point / (2 * num_points)
+            
+            self.cc_point = (f_point + 2 * e_point + (num_points - 3) * self.coor) / num_points
+            
+    def on_boundary(self):
+        
+        for e in self.adj_edges:
+            if e.on_boundary():
+                return True
+        
+        return len(self.adj_faces) == 0
+        
+    def get_val(self):
+        return self.coor
+
+class Edge(TopoElement):
+
+    def __init__(self, p, q):
+        super().__init__()
+        
+        self.adj_vertices.append(p)
+        self.adj_vertices.append(q)
+
+    def on_boundary(self):
+        return len(self.adj_faces) < 2
+
+    def compute_catmull_clark_point(self):
+        
+        if self.on_boundary():
+            self.cc_point = np.sum([v.coor for v in self.adj_vertices], axis = 0)
+            self.cc_point = self.cc_point / len(self.adj_vertices)
+        else:
+            self.cc_point = np.sum([v.coor for v in self.adj_vertices], axis = 0)
+            self.cc_point += np.sum([f.cc_point for f in self.adj_faces], axis = 0)
+            self.cc_point = self.cc_point / (len(self.adj_faces) + 2)
+            
+    def compute_doo_sabin_point(self):
+        
+            self.ds_point = np.sum([v.coor for v in self.adj_vertices], axis = 0)
+            self.ds_point = self.ds_point / len(self.adj_vertices)
+        
+    
+
+class Face(TopoElement):
+
+    def __init__(self, vertex_list, edge_list):
+        super().__init__()
+        
+        self.adj_vertices = vertex_list[:]
+        self.adj_edges = edge_list[:]
+        
+    def compute_catmull_clark_point(self):
+    
+        self.cc_point = self.get_average_point()
+      
+    def compute_doo_sabin_face_point(self):
+    
+        self.ds_point = self.get_average_point()
+        
+    def get_average_point(self):
+    
+        avg_point = np.sum([v.coor for v in self.adj_vertices], axis = 0)
+        avg_point = avg_point / len(self.adj_vertices)
+        
+        return avg_point
+        
+    def compute_doo_sabin_points(self):
+    
+        self.ds_points = []
+        
+        for i in range(len(self.adj_vertices)):
+            
+            if self.adj_vertices[i].on_boundary():
+                point = self.adj_vertices[i].coor[:]
+            
+                self.ds_points.append(point)
+            else:
+                e1_p = self.adj_edges[(i - 1) % len(self.adj_vertices)].ds_point
+                e2_p = self.adj_edges[i].ds_point
+                v_p = self.adj_vertices[i].coor
+                f_p = self.ds_point
+                
+                point = (e1_p + e2_p + v_p + f_p) / 4
+            
+                self.ds_points.append(point)
+        
+        
+        
---
+++ subdivide_animate.py
@@ -0,0 +1,303 @@
+import numpy as np
+import os
+import random
+import time
+import vtk
+import sys
+import argparse
+import cv2 as cv
+from vtk.util.numpy_support import vtk_to_numpy
+
+import mesh
+
+def generate_polydata(surface_mesh):
+
+    vertices = surface_mesh.vertices
+    polygons = surface_mesh.faces
+
+    points = vtk.vtkPoints()
+    
+    for v in vertices:
+        p = v.get_val()
+        points.InsertNextPoint(p[0], p[1], p[2])
+
+    vtk_polygons = vtk.vtkCellArray()
+
+    for p in polygons:
+        
+        vtk_polygon = vtk.vtkPolygon()
+        vtk_polygon.GetPointIds().SetNumberOfIds(len(p.adj_vertices))
+        
+        for i in range(len(p.adj_vertices)):
+            vtk_polygon.GetPointIds().SetId(i, p.adj_vertices[i].id)
+
+        vtk_polygons.InsertNextCell(vtk_polygon)
+
+    polygonPolyData = vtk.vtkPolyData()
+    polygonPolyData.SetPoints(points)
+    polygonPolyData.SetPolys(vtk_polygons)
+
+    return polygonPolyData
+
+def update_polydata_from_animate_mesh(polydata, mesh_animate, t):
+    
+    for i in range(len(mesh_animate.vertices)):
+        p = mesh_animate.vertices[i].get_val(t)
+        polydata.GetPoints().SetPoint(i, (p[0], p[1], p[2]))
+
+
+class animate_callback():
+
+    FRAMES_PER_SUBDIVISION = 90
+    CATMULL_CLARK = 1
+    DOO_SABIN = 2
+
+    def __init__(self, mesh, subdivision_method, subdivisions, renderer_window=None, out=None):
+        self.ticks = 0
+        self.subdivide_mesh = mesh
+
+        self.animate_frames = [0]
+        
+        for i in range(subdivisions):
+            frame_count = max(int(90 / ((1.25)**i)), 10)
+            self.animate_frames.append(frame_count)
+            
+        self.animate_frames.append(60)
+        
+        self.animate_intervals = [sum(self.animate_frames[0:i + 1]) for i in range(len(self.animate_frames))]
+
+        self.max_frames = subdivisions * self.FRAMES_PER_SUBDIVISION
+        self.subdivision_method = subdivision_method
+        self.subdivision_count = 0
+        
+        self.renderer_window = renderer_window
+        self.out = out
+        
+        self.finished_animation = False
+    
+    def write_to_video_stream(self):
+        
+        window_out = vtk.vtkWindowToImageFilter()
+        window_out.SetInput(self.renderer_window)
+        window_out.Update()
+        
+        window_image = window_out.GetOutput()
+        
+        width = window_image.GetDimensions()[0]
+        height = window_image.GetDimensions()[1]
+        
+        np_image = vtk_to_numpy(window_image.GetPointData().GetScalars()).reshape(height, width, 3)
+        np_image = cv.cvtColor(np_image, cv.COLOR_BGR2RGB)
+        np_image = np.flip(np_image, 0)
+        
+        self.out.write(np_image)
+    
+    def update(self, obj, event):
+
+        if self.ticks < self.animate_intervals[-1]:
+            if self.out != None:
+                self.write_to_video_stream()
+
+            if self.ticks < self.animate_intervals[-2]:
+                if self.ticks == self.animate_intervals[self.subdivision_count]:
+                    
+                    self.subdivision_count += 1
+                    print("Computing subdivision #" + str(self.subdivision_count))
+                
+                    if self.subdivision_method == self.CATMULL_CLARK:
+                        self.subdivide_mesh, self.subdivide_animate_mesh = self.subdivide_mesh.compute_catmull_clark_surface()
+                    elif self.subdivision_method == self.DOO_SABIN:
+                        self.subdivide_mesh, self.subdivide_animate_mesh = self.subdivide_mesh.compute_doo_sabin_surface()
+
+                    self.polydata = generate_polydata(self.subdivide_animate_mesh)
+                    self.mapper = vtk.vtkPolyDataMapper()
+                    self.mapper.SetInputData(self.polydata)
+                    self.actor.SetMapper(self.mapper)
+                    
+                interval_frame = self.ticks - self.animate_intervals[self.subdivision_count - 1]
+                num_frames = self.animate_frames[self.subdivision_count]
+                    
+                update_polydata_from_animate_mesh(self.polydata, self.subdivide_animate_mesh, (interval_frame + 1) / num_frames)
+                self.polydata.GetPoints().Modified()
+            
+            iren = obj
+            iren.GetRenderWindow().Render()
+            
+            self.ticks += 1
+
+def read_off_file(filename):
+
+    reader = open(filename, 'r')
+    
+    lines = reader.readlines()
+    reader.close()
+    
+    points = []
+    polys = []
+    
+    info_split = lines[1].split()
+    
+    num_vertices = int(info_split[0])
+    num_faces = int(info_split[1])
+    
+    for line in lines[2:2 + num_vertices]:
+        line_split = line.split()
+        point = [float(x) for x in line_split]
+        points.append(np.array(point))
+    
+    for line in lines[2 + num_vertices:2 + num_vertices + num_faces]:
+        line_split = line.split()
+        num_face_vertices = int(line_split[0])
+        face = [int(x) for x in line_split[1:num_face_vertices + 1]]
+        polys.append(face)
+        
+    return points, polys
+
+def read_obj_file(filename):
+
+    reader = open(filename, 'r')
+    
+    lines = reader.readlines()
+    
+    reader.close()
+    
+    points = []
+    polys = []
+    
+    for line in lines:
+            
+        line_split = line.split()
+    
+        if len(line_split) == 0:
+            continue
+    
+        if line_split[0] == "v":
+            #vertex
+            point = [float(x) for x in line_split[1:]]
+            
+            points.append(np.array(point))
+            
+        elif line_split[0] == "f":
+            #face
+            f = [int(x.split("/")[0]) - 1 for x in line_split[1:]]
+            
+            polys.append(f)
+            
+    return points, polys
+                
+#parse arguments
+
+parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument("mesh_file", help="an .off or .obj mesh file to subdivide")
+parser.add_argument("-a", type=int, help="""subdivision algorithm to visualize
+\tSubdivision algorithms:
+\t\t1: Catmull-Clark
+\t\t2: Doo-Sabin""")
+parser.add_argument("-d", type=int, help="number of subdivisions to animate")
+parser.add_argument("-v", help="video file to output animation to")
+args = parser.parse_args()
+
+filename = args.mesh_file
+subdivision_method = 1
+subdivisions = 3
+video_filename = ""
+
+if args.a:
+    subdivision_method = args.a
+    
+if args.d:
+    subdivisions = args.d
+
+if args.v:
+    video_filename = args.v
+
+#output argument information
+
+print("Mesh file: " + filename)
+
+if subdivision_method == 1:
+    print("Subdivision algorithm: Catmull-Clark")
+elif subdivision_method == 2:
+    print("Subdivision algorithm: Doo-Sabin")
+
+print("Number of subdivisions to be animated: " + str(subdivisions))
+
+if video_filename == "":
+    print("Not outputting to video file")
+else:
+    print("Outputting to video to file " + video_filename)
+
+#create initial mesh
+
+filename_suffix = filename.split(".")[-1]
+
+points = []
+polys = []
+
+if filename_suffix == "off":
+    points, polys = read_off_file(filename)
+elif filename_suffix == "obj":
+    points, polys = read_obj_file(filename)
+
+subdivide_mesh = mesh.Mesh()
+subdivide_animate_mesh = mesh.Mesh()
+
+for poly in polys:
+
+    poly_points = []
+    
+    for i in poly:
+        poly_points.append(points[i])
+
+    subdivide_mesh.add_face_by_points(poly_points)
+
+#configure vtk window
+
+width = 1800
+height = 1800
+
+renderer = vtk.vtkRenderer()
+render_window = vtk.vtkRenderWindow()
+render_window.SetSize(width, height)
+render_window.SetWindowName("Surface Subdivision")
+render_window.AddRenderer(renderer)
+render_window_interactor = vtk.vtkRenderWindowInteractor()
+render_window_interactor.SetRenderWindow(render_window)
+
+actor = vtk.vtkActor()
+
+polydata = generate_polydata(subdivide_mesh)
+mapper = vtk.vtkPolyDataMapper()
+mapper.SetInputData(polydata)
+actor.SetMapper(mapper)
+
+polydata = generate_polydata(subdivide_mesh)
+mapper = vtk.vtkPolyDataMapper()
+mapper.SetInputData(polydata)
+actor.SetMapper(mapper)
+
+actor.SetMapper(mapper)
+actor.GetProperty().SetColor(.75, .75, 1)
+actor.GetProperty().SetEdgeColor(0, 0, 0)
+actor.GetProperty().SetLineWidth(3)
+actor.GetProperty().SetEdgeVisibility(True)
+
+renderer.AddActor(actor)
+render_window.Render()
+
+render_window_interactor.Initialize()
+
+out = None
+
+if video_filename != "":
+    out = cv.VideoWriter(video_filename, cv.VideoWriter_fourcc('m','p','4','v'), 30.0, (width, height), 1)
+
+cb = animate_callback(subdivide_mesh, subdivision_method, subdivisions, render_window, out)
+cb.actor = actor
+render_window_interactor.AddObserver('TimerEvent', cb.update)
+timerId = render_window_interactor.CreateRepeatingTimer(33);
+
+render_window_interactor.GetInteractorStyle().SetCurrentStyleToTrackballCamera()
+render_window_interactor.Start()
+
+
